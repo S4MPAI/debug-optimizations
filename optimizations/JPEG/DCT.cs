b9f2b41952e2ecb8b964b3bcc1ce96a6e427f4fa
Artyom <flekshum@gmail.com>
2025-03-02 14:24:22 +0500

    optimize DCT2D

--- optimizations/JPEG/DCT.cs
+++ optimizations/JPEG/DCT.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using JPEG.Utilities;
 
 namespace JPEG;
 
@@ -9,12 +8,16 @@ public class DCT
 	private static double[,] _cosineTable;
 	private static double[] _alphaTable;
 	private static double _beta;
+	private static double[,] DCT2DTemp;
+	private static double[,] IDCT2DTemp;
 
 	public static void Initialize(int dctSize)
 	{
 		_size = dctSize;
 		_cosineTable = new double[dctSize, dctSize];
 		_alphaTable = new double[dctSize];
+		DCT2DTemp = new double[dctSize, dctSize];
+		IDCT2DTemp = new double[dctSize, dctSize];
 		_beta = 2d / dctSize;
 		for (var x = 0; x < dctSize; x++)
 		{
@@ -28,36 +31,51 @@ public class DCT
 	{
 		var coeffs = new double[_size, _size];
 
-		MathEx.LoopByTwoVariables(
-			0, _size,
-			0, _size,
-			(u, v) =>
+		for (var u = 0; u < _size; u++)
+		{
+			for (var y = 0; y < _size; y++)
 			{
-				var sum = MathEx
-					.SumByTwoVariables(
-						0, _size,
-						0, _size,
-						(x, y) => input[x, y] * _cosineTable[x, u] * _cosineTable[y, v]);
+				var sum = 0.0;
+				for (var x = 0; x < _size; x++)
+					sum += input[x, y] * _cosineTable[x, u];
+				DCT2DTemp[y, u] = sum;
+			}
+		}
 
+		for (var u = 0; u < _size; u++)
+		{
+			for (var v = 0; v < _size; v++)
+			{
+				var sum = 0.0;
+				for (var y = 0; y < _size; y++)
+					sum += DCT2DTemp[y, u] * _cosineTable[y, v];
 				coeffs[u, v] = sum * _beta * _alphaTable[u] * _alphaTable[v];
-			});
+			}
+		}
 
 		return coeffs;
 	}
 
 	public static void IDCT2D(double[,] coeffs, double[,] output)
 	{
+		for (var y = 0; y < _size; y++)
+		{
+			for (var u = 0; u < _size; u++)
+			{
+				var sum = 0.0;
+				for (var v = 0; v < _size; v++)
+					sum += coeffs[u, v] * _cosineTable[y, v] * _alphaTable[v];
+				IDCT2DTemp[y, u] = sum * _alphaTable[u];
+			}
+		}
+
 		for (var x = 0; x < _size; x++)
 		{
 			for (var y = 0; y < _size; y++)
 			{
-				var sum = MathEx
-					.SumByTwoVariables(
-						0, _size,
-						0, _size,
-						(u, v) =>
-							coeffs[u, v] * _cosineTable[x, u] * _cosineTable[y, v] * _alphaTable[u] * _alphaTable[v]);
-
+				var sum = 0.0;
+				for (var u = 0; u < _size; u++)
+					sum += IDCT2DTemp[y, u] * _cosineTable[x, u];
 				output[x, y] = sum * _beta;
 			}
 		}
